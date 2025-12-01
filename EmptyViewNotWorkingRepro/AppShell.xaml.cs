a3ce0809054768920059a666be760d92baff75f9
rrbabbb <davidnagler@pm.me>
2025-12-01 11:01:30 +0100

    Add project files.

---
+++ EmptyViewNotWorkingRepro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EmptyViewNotWorkingRepro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EmptyViewNotWorkingRepro/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace EmptyViewNotWorkingRepro
+{
+  public partial class MainPage : ContentPage
+  {
+
+    public MainPage()
+    {
+      InitializeComponent();
+
+      EmptyCollection.ItemsSource = new List<string>();
+      NullCollection.ItemsSource = null;
+      ValuesCollection.ItemsSource = new List<string>() { "string1", "string2", "string3" };
+    }
+  }
+}
